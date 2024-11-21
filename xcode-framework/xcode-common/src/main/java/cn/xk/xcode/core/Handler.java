37d969d9ad578d46691ac3d565bdc2669f88be19
arvin <1347459620@qq.com>
2024-11-21 17:07:36 +0800

    处理器注解

---
+++ xcode-framework/xcode-common/src/main/java/cn/xk/xcode/core/Handler.java
@@ -0,0 +1,23 @@
+package cn.xk.xcode.core;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author xuk
+ * @Date 2024/11/21 10:27
+ * @Version 1.0.0
+ * @Description Handler
+ **/
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Handler {
+    @AliasFor(
+            annotation = Component.class
+    )
+    String value() default "";
+}

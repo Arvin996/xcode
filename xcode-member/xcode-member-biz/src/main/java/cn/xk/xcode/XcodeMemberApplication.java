66cf4d5342e1f3a51d24e8a2dcc993374e674c5a
arvin <1347459620@qq.com>
2024-11-21 17:08:51 +0800

    xk-sys-learn改为xcode

--- xcode-member/xcode-member-biz/src/main/java/cn/xk/xcode/XcodeMemberApplication.java
+++ xcode-member/xcode-member-biz/src/main/java/cn/xk/xcode/XcodeMemberApplication.java
@@ -3,11 +3,16 @@ package cn.xk.xcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Author xuk
+ * @Date 2024/11/18 15:20
+ * @Version 1.0.0
+ * @Description XcodeMemberApplication
+ **/
 @SpringBootApplication
-public class XkSysLearnWebsocketServerApplication {
+public class XcodeMemberApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(XkSysLearnWebsocketServerApplication.class, args);
+        SpringApplication.run(XcodeMemberApplication.class, args);
     }
-
 }
--- xcode-member/xcode-member-biz/src/main/java/cn/xk/xcode/generator/AllMemberCodeGenerator.java
+++ xcode-member/xcode-member-biz/src/main/java/cn/xk/xcode/generator/AllMemberCodeGenerator.java
@@ -21,11 +21,11 @@ public class AllMemberCodeGenerator implements ICodeGenerator {
                 .entityGenerateEnable(true)
                 .serviceImplGenerateEnable(true)
                 .tableDefGenerateEnable(true)
-                .sourceDir(System.getProperty("user.dir") + "/own_module/xk-sys-learn/xk-sys-learn-member/xk-sys-learn-member-biz/src/main/java")
+                .sourceDir(System.getProperty("user.dir") + "/own_module/xcode-parent/xcode-member/xcode-member-biz/src/main/java")
                 .tables(new String[]{"member_address", "member_config", "member_experience_record", "member_group"
                         , "member_level", "member_level_change_record", "member_login_record", "member_point_record"
                         , "member_sign", "member_sign_record", "member_tag", "member_user"})
-                .mapperXmlPackage(System.getProperty("user.dir") + "/own_module/xk-sys-learn/xk-sys-learn-member/xk-sys-learn-member-biz/src/main/resources/mapper")
+                .mapperXmlPackage(System.getProperty("user.dir") + "/own_module/xcode-parent/xcode-member/xcode-member-biz/src/main/resources/mapper")
                 .entityPackage("cn.xk.xcode.entity.po")
                 .tableDefPackage("cn.xk.xcode.entity.def")
                 .entityClassSuffix("Po").build();
@@ -37,7 +37,7 @@ public class AllMemberCodeGenerator implements ICodeGenerator {
                 .builder()
                 .dbIp("127.0.0.1")
                 .dbPort("3306")
-                .dbName("xk-learn-member")
+                .dbName("xcode-member")
                 .username("root")
                 .password("123456")
                 .build();

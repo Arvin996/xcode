3f8b5d06496130a81239ba9dbba2204cee188a75
arvin <1347459620@qq.com>
2025-05-16 17:03:06 +0800

    fix

--- xcode-websocket-server/src/main/java/cn/xk/xcode/XcodeWebsocketServerApplication.java
+++ xcode-websocket-server/src/main/java/cn/xk/xcode/XcodeWebsocketServerApplication.java
@@ -2,8 +2,11 @@ package cn.xk.xcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
 public class XcodeWebsocketServerApplication {
 
     public static void main(String[] args) {
